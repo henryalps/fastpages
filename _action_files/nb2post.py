bef174f8164b5ec015e9bd1697eac32b7cf8050f
henryalps <henryalps@users.noreply.github.com>
2020-06-06 12:49:04 +0800

    optimize jekyll build time: save local image

--- _action_files/nb2post.py
+++ _action_files/nb2post.py
@@ -40,7 +40,7 @@ def _update_att_ref_local(line, mime, img):
     if not title: title = ""
     print(img, mime)
     print(img[-8:], mime.split('/')[1])
-    title = "images/{}.{}".format(img[-8:], mime.split('\/')[1])
+    title = "images/{}.{}".format(img[-8:], mime.split('/')[1])
     print(title)
     with open(title, "wb") as fh:
         fh.write(base64.decodebytes(img))
